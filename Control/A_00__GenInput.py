0e2086468be2838535c413e13aa03808b45f530d
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-09-23 17:44:08 +0200

    Add files via upload

--- Control/A_00__GenInput.py
+++ Control/A_00__GenInput.py
@@ -7,6 +7,13 @@ import Core.C_00__GenConstants as GC
 # --- Input: Debug Info -------------------------------------------------------
 levelDebugOut = 2   # level of debug output (0: no debug output)
 
+# --- Input: Initial state ----------------------------------------------------
+sStIni = GC.S_ST_TRANS_AT5G49770_NRT2P1      # initial state ID string
+                                        # GC.S_ST_INT_AT5G49770_NRT2P1
+                                        # GC.S_ST_TRANS_AT5G49770_NRT2P1
+                                        # GC.S_ST_INT_NAR2P1_NRT2P1
+                                        # GC.S_ST_TRANS_NAR2P1_NRT2P1
+
 # --- Input: General ----------------------------------------------------------
 nDigObj = 3         # number of digits reserved for all input objects
 cMode = GC.M_DETER  # GC.M_DETER / GC.M_STOCH
@@ -17,6 +24,8 @@ nDPlots = 'Plots'                   # name of directory for plots
 # --- create input dictionary -------------------------------------------------
 dictInpG = {# --- Input: Debug Info
             'lvlDbg': levelDebugOut,
+            # --- Input: Initial state
+            'sStIni': sStIni,
             # --- Input: General
             'nDigObj': nDigObj,
             'Mode': cMode,
