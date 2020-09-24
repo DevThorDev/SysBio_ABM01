bda3677b771ec65a56e4a942c2b2383e5c79a82c
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-09-24 11:55:13 +0200

    Add files via upload

--- Control/A_00__GenInput.py
+++ Control/A_00__GenInput.py
@@ -8,11 +8,11 @@ import Core.C_00__GenConstants as GC
 levelDebugOut = 2   # level of debug output (0: no debug output)
 
 # --- Input: Initial state ----------------------------------------------------
-sStIni = GC.S_ST_TRANS_AT5G49770_NRT2P1      # initial state ID string
-                                        # GC.S_ST_INT_AT5G49770_NRT2P1
-                                        # GC.S_ST_TRANS_AT5G49770_NRT2P1
-                                        # GC.S_ST_INT_NAR2P1_NRT2P1
-                                        # GC.S_ST_TRANS_NAR2P1_NRT2P1
+sStIni = GC.S_ST_D_TRANS_NAR2P1_NRT2P1    # initial state ID string
+                                        # GC.S_ST_A_INT_AT5G49770_NRT2P1
+                                        # GC.S_ST_B_TRANS_AT5G49770_NRT2P1
+                                        # GC.S_ST_C_INT_NAR2P1_NRT2P1
+                                        # GC.S_ST_D_TRANS_NAR2P1_NRT2P1
 
 # --- Input: General ----------------------------------------------------------
 nDigObj = 3         # number of digits reserved for all input objects
