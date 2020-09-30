303693f4b0b18ff14b1ee24fe77276e68a640176
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-09-30 17:51:35 +0200

    Add files via upload

---
+++ ObjInput/B_001__Molecule.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# --- B_001__Molecule.py ------------------------------------------------------
+###############################################################################
+
+# --- general -----------------------------------------------------------------
+strOType = 'Molecule'
+strNSpec = 'Molecule Base'
+strCS = 'Mol'
+strCL = 'Molecule Base'
+
+# --- names of directories and files ------------------------------------------
+sD_SMo = '80_SMo'
+sF_SMo = 'ConcSMo'
+
+# --- create input dictionary -------------------------------------------------
+dIO = {'strOType': strOType,
+       'strNSpec': strNSpec,
+       'strCS': strCS,
+       'strCL': strCL,
+       # --- names of directories and files
+       'sD_SMo': sD_SMo,
+       'sF_SMo': sF_SMo}
+###############################################################################
