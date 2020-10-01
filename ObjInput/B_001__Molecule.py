036de83107c04171ebd161f6b77e8c567dc41d68
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-10-01 18:36:56 +0200

    Add files via upload

--- ObjInput/B_001__Molecule.py
+++ ObjInput/B_001__Molecule.py
@@ -11,7 +11,21 @@ strCL = 'Molecule Base'
 
 # --- names of directories and files ------------------------------------------
 sD_SMo = '80_SMo'
-sF_SMo = 'ConcSMo'
+sF_SMo = 'SMo'
+
+# --- graphics parameters molecule concentrations plot ------------------------
+sPlt_Conc = 'Conc'     # name of the plot and key of the input dict
+title_Conc = None       # title of plot
+xLbl_Conc = 'Time step' # x-label of plot
+yLbl_Conc = 'Concentration [mMol]'    # y-label of plot
+tpMark_Conc = 'x'       # marker type of plot
+szMark_Conc = 1         # marker size of plot
+ewMark_Conc = 1         # marker edge width of plot
+ecMark_Conc = (1., 0., 0.)     # marker edge colour of plot
+fcMark_Conc = (1., 0.5, 0.)    # marker face colour of plot
+styLn_Conc = 'solid'    # line style of plot
+wdthLn_Conc = 1         # line width of plot
+colLn_Conc = (1., 0.5, 0.)     # line colour of plot
 
 # --- create input dictionary -------------------------------------------------
 dIO = {'strOType': strOType,
@@ -20,5 +34,19 @@ dIO = {'strOType': strOType,
        'strCL': strCL,
        # --- names of directories and files
        'sD_SMo': sD_SMo,
-       'sF_SMo': sF_SMo}
+       'sF_SMo': sF_SMo,
+       # --- graphics parameters molecule concentrations plot
+       'sPlt_Conc': sPlt_Conc,
+       sPlt_Conc: {'title': title_Conc,
+                   'xLbl': xLbl_Conc,
+                   'yLbl': yLbl_Conc,
+                   'tpMark': tpMark_Conc,
+                   'szMark': szMark_Conc,
+                   'ewMark': ewMark_Conc,
+                   'ecMark': ecMark_Conc,
+                   'fcMark': fcMark_Conc,
+                   'styLn': styLn_Conc,
+                   'wdthLn': wdthLn_Conc,
+                   'colLn': colLn_Conc},
+       }
 ###############################################################################
