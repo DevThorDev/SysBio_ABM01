c308c5eeaa51b56194ff1f0a9deacf1e9506a09d
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-10-05 17:32:44 +0200

    Add files via upload

--- ObjInput/B_201__NRT2p1.py
+++ ObjInput/B_201__NRT2p1.py
@@ -9,12 +9,20 @@ strOType = 'Large protein'
 strNSpec = 'NRT2.1'
 strCS = 'NRT2.1'
 strCL = 'NRT2p1'
+dParP_S21_KAs3_Pyl = {'prMin': 0, 'prMax': 0.2,
+                      'B': 10, 'C': 1, 'D': 0.025}
+dParP_S21_PAs3_DePyl = {'prMin': 0, 'prMax': 0.3,
+                        'B': 15, 'C': 0.8, 'D': 0.04}
+dParP_S28_KAsX_Pyl = {'prMin': 0, 'prMax': 0.3,
+                      'B': 5, 'C': 1.5, 'D': 0.02}
+dParP_S28_PAs4_DePyl = {'prMin': 0, 'prMax': 0.4,
+                        'B': 12, 'C': 2.75, 'D': 0.01}
 dInfSpS = {'S21': {'Stat': GC.B_NOT_PYL,
-                   'Pyl': [GC.ID_KAS_3],
-                   'DePyl': [GC.ID_PAS_3]},
+                   'Pyl': [(GC.ID_KAS_3, dParP_S21_KAs3_Pyl)],
+                   'DePyl': [(GC.ID_PAS_3, dParP_S21_PAs3_DePyl)]},
            'S28': {'Stat': GC.B_NOT_PYL,
-                   'Pyl': [GC.ID_KAS_X],
-                   'DePyl': [GC.ID_PAS_4]}}
+                   'Pyl': [(GC.ID_KAS_X, dParP_S28_KAsX_Pyl)],
+                   'DePyl': [(GC.ID_PAS_4, dParP_S28_PAs4_DePyl)]}}
 
 # --- create input dictionary -------------------------------------------------
 dIO = {'strOType': strOType,
