4a896042fac903ece8fa43464965c641061a8f70
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-10-08 17:35:17 +0200

    Add files via upload

--- ObjInput/B_099__System.py
+++ ObjInput/B_099__System.py
@@ -11,7 +11,7 @@ strCS = 'Sys'
 strCL = 'System'
 
 # --- dictionaries containing state and state transition info -----------------
-dNStates = {GC.S_ST_A_INT_AT5G49770_NRT2P1: 2,
+dNStaObj = {GC.S_ST_A_INT_AT5G49770_NRT2P1: 2,
             GC.S_ST_B_TRANS_AT5G49770_NRT2P1: 1,
             GC.S_ST_C_INT_NAR2P1_NRT2P1: 3,
             GC.S_ST_D_TRANS_NAR2P1_NRT2P1: 1}
@@ -20,13 +20,20 @@ dRRC = {'A_B': 0.1,
         'C_D': 0.15,
         'D_A': 0.02}
 
+# --- path, directory and file names ------------------------------------------
+sD_Sys = '99_Sys'
+sF_SysEvo = 'SysEvo'
+
 # --- create input dictionary -------------------------------------------------
 dIO = {'strOType': strOType,
        'strNSpec': strNSpec,
        'strCS': strCS,
        'strCL': strCL,
        # --- dictionaries containing state and state transition info
-       'dNStates': dNStates,
-       'dRRC': dRRC}
+       'dNStaObj': dNStaObj,
+       'dRRC': dRRC,
+       # --- path, directory and file names
+       'sD_Sys': sD_Sys,
+       'sF_SysEvo': sF_SysEvo}
 
 ###############################################################################
