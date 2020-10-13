00860e6503a4a1276aa3ba5ac64c5b60eacbf413
Thorsten <47179219+DevThorDev@users.noreply.github.com>
2020-10-13 14:40:35 +0200

    Add files via upload

--- M_0__Main.py
+++ M_0__Main.py
@@ -21,38 +21,10 @@ inDG = InputData(dictInpG)
 inDG.addObjTps(S_D_OBJINP)
 print('Added object types.')
 # -----------------------------------------------------------------------------
-# cSystem = MF.iniSystem(inDG)
-# cSystem.printSystem()
-
-# -----------------------------------------------------------------------------
-# cState, cSystem = MF.initialState(inDG)
-# # cSystem.printSystemDetails()
-# print('~'*30, 'INITIAL STATE', '~'*30)
-# cState.printStateDetails()
-# print('~'*30, 'STATE A', '~'*30)
-# cState.to_St_A_Int_AT5G49770_NRT2p1(inDG)
-# cState.printStateDetails()
-# print('~'*30, 'STATE B', '~'*30)
-# cState.to_St_B_Trans_AT5G49770_NRT2p1(inDG)
-# cState.printStateDetails()
-# print('~'*30, 'STATE C', '~'*30)
-# cState.to_St_C_Int_NAR2p1_NRT2p1(inDG)
-# cState.printStateDetails()
-# print('~'*30, 'STATE D', '~'*30)
-# cState.to_St_D_Trans_NAR2p1_NRT2p1(inDG)
-# cState.printStateDetails()
-
-# -----------------------------------------------------------------------------
 cSystem = System(inDG) 
 cSystem.evolveOverTime()
 cSystem.printSystemIDs()
 # cSystem.printSystemObj()
-
-# dStoch = {GC.ID_NO3_1M: {GC.S_CONC_INI: RNG().uniform(4.6, 5.8, nStaObj),
-#                          GC.S_PER_CONC_CH: RNG().uniform(10, 200, nStaObj)},
-#           GC.ID_H2PO4_1M: {GC.S_CONC_INI: RNG().uniform(1.7, 2.2, nStaObj)}}
-
-# MF.evolve_TimeSteps(inDG, nObj = nStaObj, ddVOvwr = dStoch)
 
 # -----------------------------------------------------------------------------
 
